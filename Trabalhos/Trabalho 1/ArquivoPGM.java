de1737f57968dc54a126eb8661f4a721b53d24cf
Zéh Neto <josemagalhaesnt@gmail.com>
2017-05-05 10:18:55 -0300

    Feito o escopo do trabalho e declarações

---
+++ Trabalhos/Trabalho 1/ArquivoPGM.java
@@ -0,0 +1,43 @@
+public class ArquivoPGM {
+	
+	/** Atributos **/
+
+	private int largura
+	private int altura;
+	private int imagem[];
+
+	/** Getters e Setters **/
+
+	public int getLargura(){
+		return largura;
+	}
+
+	public void setLargura(int largura){
+		this.largura = largura;
+	}
+
+	public int getAltura(){
+		return altura;
+	}
+
+	public void setAltura(int altura){
+		this.altura = altura;
+	}
+
+	/** Métodos da Classe **/
+
+	public boolean carregaImagem(String nome){
+
+		/** Verificar se a imagem existe (true) ou não (false) **/
+
+		/** Caso exista, ler arquivo linha a linha e retornar atributos **/
+
+	}
+
+	public boolean escrevePGM (String nome){
+		/** Criar um arquivo PGM com o nome dado **/
+
+		/** Caso der certo, retornar true, caso contrário false **/
+	}
+
+}
